7c1c63e644e14662763ffbe09b63f9c06c8d3b76
Marek Matczak <mmatczak@autograf.pl>
2020-07-22 17:08:24 +0200

    tests

--- src/app/book/book-details/book-details.component.spec.ts
+++ src/app/book/book-details/book-details.component.spec.ts
@@ -1,25 +1,41 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {BookOverviewComponent} from '../book-overview/book-overview.component';
+import {BookDetailsComponent} from './book-details.component';
+import {Book} from '../book.model';
 
-import { BookDetailsComponent } from './book-details.component';
-
-describe('BookDetailsComponent', () => {
-  let component: BookDetailsComponent;
-  let fixture: ComponentFixture<BookDetailsComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BookDetailsComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BookDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+fdescribe('BookDetailsComponent', () => {
+  describe('(class)', () => {
+    it('fires an event on book change', () => {
+      // given
+      const eventMock = {
+        preventDefault: jasmine.createSpy('preventDefault'),
+        target: jasmine.createSpyObj(['querySelector'])
+      } as any;
+      eventMock.target.querySelector.and.callFake((selector: string) => {
+        const value = selector === '#author' ? 'New Author' : 'New Title';
+        return {value};
+      });
+      const component = new BookDetailsComponent();
+      component.book = {
+        id: 0,
+        title: 'Test title',
+        author: 'Test author'
+      };
+      component.bookChange.subscribe(
+        (changedBook: Book) => {
+          // then
+          expect(eventMock.preventDefault).toHaveBeenCalled();
+          expect(eventMock.target.querySelector).toHaveBeenCalledTimes(2);
+          expect(changedBook.id).toBe(0);
+          expect(changedBook.author).toBe('New Author');
+          expect(changedBook.title).toBe('New Title');
+        }
+      );
+      // when
+      component.notifyOnBookChange(eventMock);
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('(DOM)', () => {
+
   });
 });
